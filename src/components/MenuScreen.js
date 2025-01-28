183a1b0cbd912ac6dadeeb48983df67785e39e57
SamuelMorais <samuelmorais552@gmail.com>
2025-01-28 14:40:50 -0300

    feat: Components to select number of checks/submissions

--- src/components/MenuScreen.js
+++ src/components/MenuScreen.js
@@ -3,25 +3,47 @@ import { PreviewSudokuBoard } from "./SudokuBoard.js";
 import { useState } from "react";
 import "./MenuScreen.css"
 
+const DEFAULT_SETTINGS = {boardId : 0, numberOfChecks : 3, numberOfSubmits : 3}
+
+function SettingsSelector({caption, updateSetting}) {
+  return (
+    <div>
+      <input name="number-input" type="number" min={0} max={10} onChange={(event) => updateSetting(event.target.value)}></input>
+      <label htmlFor="number-imput">
+        {caption}
+      </label>
+    </div>
+  )
+}
 
 export default function MenuScreen({ boards, startGame, updateSettings }) {
+
+  const [currentSettings, setCurrentSettings] = useState(DEFAULT_SETTINGS)
 
   const [currentBoard, setcurrentBoard] = useState(0);
 
   function nextBoard() {
-    if (currentBoard === boards.length - 1) {
-      setcurrentBoard(0);
+    if (currentSettings.boardId === boards.length - 1) {
+      setCurrentSettings({...currentSettings, boardId : 0});
     } else {
-      setcurrentBoard(currentBoard + 1);
+      setCurrentSettings({ ...currentSettings, boardId: currentBoard + 1 });
     }
   }
 
   function prevBoard() {
-    if (currentBoard === 0) {
-      setcurrentBoard(boards.length - 1);
+    if (currentSettings.boardId === 0) {
+      setCurrentSettings({ ...currentSettings, boardId: boards.length - 1});
     } else {
-      setcurrentBoard(currentBoard - 1);
+      setCurrentSettings({ ...currentSettings, boardId: currentBoard - 1 });
     }
+  }
+
+  function updateNumOfChecks(number) {
+    setCurrentSettings({...currentSettings, numberOfChecks : number})
+  }
+
+  function updateNumOfSubmits(number) {
+    setCurrentSettings({ ...currentSettings, numberOfSubmits: number })
   }
 
   return (
@@ -30,6 +52,8 @@ export default function MenuScreen({ boards, startGame, updateSettings }) {
         <h1>Sudoku</h1>
       </header>
       <main>
+        <SettingsSelector caption={"Number of Checks"} updateSetting={updateNumOfChecks}></SettingsSelector>
+        <SettingsSelector caption={"Number of Submits"} updateSetting={updateNumOfSubmits}></SettingsSelector>
         <span>
           Dificulty: {boards[currentBoard].dificulty}
         </span>
@@ -42,8 +66,7 @@ export default function MenuScreen({ boards, startGame, updateSettings }) {
         </span>
         <button onClick={() => {
           // TODO: Replace the mock attributes below
-          const newSettings = { boardId : boards[currentBoard].id, numberOfChecks : 2, numberOfSubmits : 2}
-          updateSettings(newSettings);
+          updateSettings(currentSettings);
           startGame();
         }}> Select Puzzle </button>
       </main>
