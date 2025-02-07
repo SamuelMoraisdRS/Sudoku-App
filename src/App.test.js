c747d415de577768cafd62c29a79b308f620a001
SamuelMorais <samuelmorais552@gmail.com>
2025-02-07 10:43:47 -0300

    feat: Test service added to docker-compose file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// TODO: Add Tests
+
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sudoku/i);
   expect(linkElement).toBeInTheDocument();
 });
